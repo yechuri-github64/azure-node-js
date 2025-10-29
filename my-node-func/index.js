967999de7ce3edcefe7eb85b03653689bd84c281
yechuri-github64 <81133261+yechuri-github64@users.noreply.github.com>
2025-10-29 11:37:45 +0530

    Delete my-node-func directory

--- my-node-func/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (context, req) {
-  const name = req.query.name || (req.body && req.body.name);
-  context.log("Function executed with name:", name);
-  context.res = {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-    body: { message: `Hello, ${name || "World"}! from Azure Function 🚀` }
-  };
-};
